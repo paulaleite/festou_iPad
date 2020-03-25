4d1a5f5b23ec844c566634593c0ea2efa5d53e81
Paula Leite <paula.t.leite@icloud.com>
2020-03-25 17:48:14 -0300

    This app now has only support for iPad and also can you multiple windows

---
+++ PlanejadorDeFesta/PlanejadorDeFesta/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  PlanejadorDeFesta
+//
+//  Created by Paula Leite on 25/03/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Since you need to support iOS 12, wrap this class in an availability check. It will only compile for iOS 13.
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  // The operating system populates window, but it has to be present.
+  var window: UIWindow?
+}
+
